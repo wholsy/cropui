5986f05ef4c41fabf14d1480a013d8ea379dcfca
yueny09 <yueny09@163.com>
2016-11-28 13:01:49 +0800

    脏词处理

--- service/src/main/java/com/yueny/blog/service/common/Configuration.java
+++ service/src/main/java/com/yueny/blog/service/common/Configuration.java
@@ -1,13 +1,11 @@
 package com.yueny.blog.service.common;
 
-import java.io.File;
-import java.io.FileFilter;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.yueny.rapid.lang.util.utility.ConfigurationClassLoader;
 
 /**
  * @author yueny09 <deep_blue_yang@163.com>
@@ -15,50 +13,36 @@ import org.slf4j.LoggerFactory;
  * @DATE 2016年11月26日 下午2:59:53
  *
  */
-public abstract class Configuration {
+public abstract class Configuration extends ConfigurationClassLoader {
 	/**
 	 * 日志记录器
 	 */
 	private static final Logger logger = LoggerFactory.getLogger(Configuration.class);
 	private static final Properties properties = new Properties();
 
-	private static final String system;
-
-	static {
-		system = System.getProperty("os.name").toLowerCase();
-		if (logger.isInfoEnabled()) {
-			logger.info("os.name = " + system);
-		}
-
-		final File classpath = getClasspathFile("");
-		final File[] propertyFiles = classpath.listFiles(new FileFilter() {
-			@Override
-			public boolean accept(final File file) {
-				return file.getName().endsWith(".properties") && !file.getName().startsWith("log4j");
-			}
-		});
-		for (final File propertyFile : propertyFiles) {
-			try {
-				properties.load(new FileInputStream(propertyFile));
-				if (logger.isInfoEnabled()) {
-					logger.info("load properties file success :" + propertyFile);
-				}
-			} catch (final IOException e) {
-				logger.warn("load properties file failed , skiped :" + propertyFile, e);
-			}
-		}
-	}
-
-	public static String get(final String key) {
-		return properties.getProperty(key);
-	}
-
-	public static ClassLoader getClassLoader() {
-		return Configuration.class.getClassLoader();
-	}
-
-	public static File getClasspathFile(final String path) {
-		return new File(getClassLoader().getResource(path).getFile());
-	}
+	// static {
+	// final File classpath = getClasspathFile("");
+	// final File[] propertyFiles = classpath.listFiles(new FileFilter() {
+	// @Override
+	// public boolean accept(final File file) {
+	// return file.getName().endsWith(".properties") &&
+	// !file.getName().startsWith("log4j");
+	// }
+	// });
+	// for (final File propertyFile : propertyFiles) {
+	// try {
+	// properties.load(new FileInputStream(propertyFile));
+	// if (logger.isInfoEnabled()) {
+	// logger.info("load properties file success :" + propertyFile);
+	// }
+	// } catch (final IOException e) {
+	// logger.warn("load properties file failed , skiped :" + propertyFile, e);
+	// }
+	// }
+	// }
+	//
+	// public static String get(final String key) {
+	// return properties.getProperty(key);
+	// }
 
 }
